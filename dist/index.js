34051f25ce05962c889b2f62a1a614188a6cee10
Shahriar Eshraghi <shahriareshraghi22@gmail.com>
2023-09-04 17:33:16 +0330

    log a file

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,19 @@
 #! /usr/bin/env node 
 "use strict";
-const figlet = require("figlet");
 const fs = require("fs");
+const path = require('path');
+const figlet = require("figlet");
 const { Command } = require('@commander-js/extra-typings');
 const { program } = require('commander');
-program
-    .option('--first')
-    .option('-s, --separator <char>');
-program.parse();
-const options = program.opts();
-const limit = options.first ? 1 : undefined;
-console.log(program.args[0].split(options.separator, limit));
+const filePath = path.join(__dirname, '../sample/sample.txt');
+const data = fs.readFileSync(filePath);
+console.log(filePath);
+console.log(data.toString());
+// program
+//   .option('--first')
+//   .option('-s, --separator <char>');
+// program.parse();
+// const options = program.opts();
+// const limit = options.first ? 1 : undefined;
+// console.log(program.args[0].split(options.separator, limit));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 #! /usr/bin/env node 
-const figlet = require("figlet");
 const fs = require("fs");
+const path = require('path');
+const figlet = require("figlet");
 const  {Command} = require('@commander-js/extra-typings');
 const { program } = require('commander');
 
-program
-  .option('--first')
-  .option('-s, --separator <char>');
+const filePath = path.join(__dirname,'../sample/sample.txt')
+const data = fs.readFileSync(filePath)
 
-program.parse();
+console.log(data.toString());
 
-const options = program.opts();
-const limit = options.first ? 1 : undefined;
-console.log(program.args[0].split(options.separator, limit));
+
+
+// program
+//   .option('--first')
+//   .option('-s, --separator <char>');
+
+// program.parse();
+
+// const options = program.opts();
+// const limit = options.first ? 1 : undefined;
+// console.log(program.args[0].split(options.separator, limit));
